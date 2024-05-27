e257f7c435b206d06d13525ee44581375aa0f474
scriptsorcery4 <form231123@gmail.com>
2024-05-27 04:55:00 +0200

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
+import Header from "./components/Header"
 
 function App() {
   
   return (
    <>
-   <h1 className="underline font-montserrat">test</h1>
+    <Header/>
    </>
   )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import { LuSearch, LuUser } from "react-icons/lu";
+import { Button } from './ui/button';
+
+function Header() {
+  return (
+    <div className='h-20 flex items-center font-montserrat'>
+        <div className='container flex justify-between items-center'>
+            <img  className="h-[29px]" src={logo} alt="" />
+            <ul className='flex gap-[55px] font-bold text-[#2B1B12] items-center '>
+                <li>Home</li>
+                <li>Service</li>
+                <li>Agents</li>
+                <li>Contact</li>
+            </ul>
+            <div className='flex gap-[45px] items-center'>
+                <div className='flex gap-[32px] items-center'>
+                    <LuSearch size={24} />
+                    <LuUser  size={24} />
+                </div>
+                <Button>Singup</Button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
